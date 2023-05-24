d52b57a6bd7998bac818ee76e59d28af45f85819
Jason Wu <jasonwu070721@gmail.com>
2023-05-24 14:25:29 +0800

    Add file status update

--- Main.py
+++ Main.py
@@ -18,7 +18,7 @@ import datetime
 import os
 
 IF_GET_CHECKSUM = False
-OS_TYPE = "windows" # synology, windows
+OS_TYPE = "windows"  # synology, windows
 
 con = sqlite3.connect("file_info.db")
 cur = con.cursor()
@@ -29,31 +29,32 @@ cur.execute("CREATE TABLE IF NOT EXISTS files(\
     id INTEGER PRIMARY KEY AUTOINCREMENT, \
     file_path TEXT, \
     md5, file_size TEXT, \
-    file_mtime TEXT, \
-    file_ctime TEXT, \
+    file_mtime FLOAT, \
+    file_ctime FLOAT, \
     file_extension TEXT, \
     created_at timestamp, \
     updated_at timestamp \
     )")
+
+
 class Main:
 
     def logger(self):
-        """ 獲取logger"""
+
         logger = logging.getLogger()
         if not logger.handlers:
-            # 指定logger輸出格式
+
             formatter = logging.Formatter(
                 '%(asctime)s %(levelname)-8s: %(message)s')
-            # 檔案日誌
+
             file_handler = logging.FileHandler("test.log")
-            file_handler.setFormatter(formatter)  # 可以通過setFormatter指定輸出格式
-            # 控制檯日誌
+            file_handler.setFormatter(formatter)
+
             console_handler = logging.StreamHandler(sys.stdout)
-            console_handler.formatter = formatter  # 也可以直接給formatter賦值
-            # 為logger新增的日誌處理器
+            console_handler.formatter = formatter
+
             logger.addHandler(file_handler)
             logger.addHandler(console_handler)
-            # 指定日誌的最低輸出級別，預設為WARN級別
             logger.setLevel(logging.INFO)
         return logger
 
@@ -73,25 +74,29 @@ class Main:
         md5_value = m.hexdigest()
         return md5_value
 
+    def check_file_modification(self, file_path, file_size, file_mtime):
+        _file_size = os.path.getsize(file_path)
+        _file_mtime = os.path.getmtime(file_path)
+
+        if (file_size == _file_size and file_mtime == _file_mtime):
+            return True
+
+        return False
+
     def get_file_info(self, file_path):
         md5 = None
         if IF_GET_CHECKSUM:
             md5 = self.get_md5(file_path)
         file_size = os.path.getsize(file_path)
-        file_tmtime = os.path.getmtime(file_path)
         file_mtime = os.path.getmtime(file_path)
         file_ctime = os.path.getctime(file_path)
         _, file_extension = os.path.splitext(file_path)
         file_extension = file_extension.lower()
-        
-        # file_path = os.path.normpath(file_path)
 
         file_status = {'file_path': file_path, 'md5': md5, 'file_size': file_size,
                        'file_mtime': file_mtime, 'file_ctime': file_ctime, 'file_extension': file_extension}
 
-        # print(file_status)
         return file_status
-        # file_status_list.append(file_status)
 
     def get_file_list(self, root_path):
 
@@ -104,53 +109,76 @@ class Main:
             for file in files:
                 path = os.path.join(root, file)
                 path = os.path.normpath(path)
-                # print(path)
+
                 file_list.append(path)
-        # print(file_list)
 
         return file_list
 
     def sort(self):
         print("sort")
 
-    def find_file_exist_in_db(self, file_path):
-    
+    def get_file_db(self, file_path):
+
         cur.execute(f'SELECT * FROM files WHERE file_path == "{file_path}"')
         files_db = cur.fetchall()
         con.commit()
-        
+
         return files_db
+
+    def update_file_status_in_db(self, file_path, file_id):
+
+        file_info = self.get_file_info(file_path)
+
+        file_size = file_info["file_size"]
+        file_mtime = file_info["file_mtime"]
+        file_ctime = file_info["file_ctime"]
+        updated_at = datetime.datetime.now()
+
+        cur.execute(f'UPDATE files SET \
+            file_size = {file_size},  \
+            file_mtime = {file_mtime},  \
+            file_ctime = {file_ctime},  \
+            updated_at = "{updated_at}"  \
+            WHERE id == {file_id}')
+
+        con.commit()
+        return files_db
+
 
 if __name__ == '__main__':
 
     main = Main()
-    
+
     find_dir = "/home/jason/side_project/car_repair_web_react"
 
     log = main.logger()
     file_list = main.get_file_list(find_dir)
-    
+
     file_count = 0
 
     for file_path in file_list:
-        
-        files_db = main.find_file_exist_in_db(file_path)
-        # print(files_db)
-        print(len(files_db))
-        
+
+        files_db = main.get_file_db(file_path)
         if len(files_db) > 0:
+            file_size = files_db[0][3]
+            file_mtime = files_db[0][4]
+            file_db_id = files_db[0][0]
+
+            file_is_modify = main.check_file_modification(file_path,
+                                                          file_size,
+                                                          file_mtime)
+
+            if (file_is_modify):
+                main.update_file_status_in_db(file_path, file_db_id)
+
             continue
-        
-        # file_path = file_status["file_path"]
-        # print(file)
+
         file_status = main.get_file_info(file_path)
-        
+
         file_count = file_count + 1
-        # print(datetime.datetime.now())
-        
+
         created_at = datetime.datetime.now()
-        updated_at = datetime.datetime.now()
-        
+
         insertQuery = """INSERT INTO files (
             file_path,
             md5, 
@@ -170,7 +198,8 @@ if __name__ == '__main__':
             file_status["file_ctime"],
             file_status["file_extension"],
             created_at,
-            updated_at))
+            created_at))
+
         con.commit()
 
     main.sort()
